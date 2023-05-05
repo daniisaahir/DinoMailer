42127b0088aacaebf27456960de4a21d3c9de716
Danii Saahir <131199603+daniisaahir@users.noreply.github.com>
2023-05-05 10:38:54 +0800

    UI rewrite

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$statusMessage = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the form inputs
     $to = $_POST["to"];
@@ -13,111 +14,149 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Send the email
     if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully.";
+        $statusMessage = "Email sent successfully.";
     } else {
-        echo "Email sending failed.";
+        $statusMessage = "Email sending failed.";
     }
 }
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Email Form</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,600">
     <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
         body {
-            background-color: black;
-            color: white;
-            font-family: Arial, sans-serif;
-            font-size: 16px;
-            padding: 20px;
+            background-color: #111;
+            font-family: 'Poppins', sans-serif;
+        }
+
+        .container {
+            width: 100%;
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 40px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+
+        h1 {
+            font-size: 36px;
+            font-weight: 600;
+            margin-bottom: 20px;
+            text-align: center;
+            color: #fff;
         }
 
         form {
-            margin: 20px auto;
-            max-width: 800px;
             width: 100%;
+            max-width: 500px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-color: #222;
+            border-radius: 10px;
+            padding: 40px;
+            box-shadow: 0 20px 40px rgba(0, 0, 0, 0.1);
         }
 
         label {
-            display: block;
-            margin-bottom: 5px;
+            font-size: 18px;
+            font-weight: 500;
+            margin-bottom: 10px;
+            color: #fff;
         }
 
         input[type="email"],
         input[type="text"],
         textarea {
+            width: 100%;
+            padding: 10px;
+            font-size: 16px;
+            font-weight: 400;
+            color: #fff;
             background-color: #333;
             border: none;
             border-radius: 5px;
-            color: white;
-            font-size: 16px;
-            padding: 10px;
-            width: 100%;
+            margin-bottom: 20px;
+        }
+
+        input[type="email"]:focus,
+        input[type="text"]:focus,
+        textarea:focus {
+            outline: none;
+            box-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
+        }
+
+        textarea {
+            height: 150px;
+            resize: none;
         }
 
         input[type="submit"] {
-            background-color: red;
+            background-color: #ff4444;
+            color: #fff;
+            font-size: 18px;
+            font-weight: 500;
             border: none;
             border-radius: 5px;
-            color: white;
+            padding: 10px 20px;
             cursor: pointer;
+            transition: background-color 0.2s ease-in-out;
+           input[type="submit"]:hover {
+        background-color: #ff3333;
+    }
+
+    @media screen and (max-width: 768px) {
+        form {
+            padding: 20px;
+        }
+    }
+
+    @media screen and (max-width: 480px) {
+        h1 {
+            font-size: 28px;
+        }
+
+        form {
+            max-width: 350px;
+        }
+
+        input[type="submit"] {
             font-size: 16px;
-            margin-top: 10px;
-            padding: 10px;
-            width: 100%;
         }
-
-        input[type="submit"]:hover {
-            background-color: #cc0000;
-        }
-
-        @media screen and (max-width: 600px) {
-            body {
-                font-size: 14px;
-                padding: 10px;
-            }
-
-            input[type="email"],
-            input[type="text"],
-            textarea {
-                font-size: 14px;
-                padding: 5px;
-            }
-
-            input[type="submit"] {
-                font-size: 14px;
-                padding: 5px;
-            }
-        }
-
-        /* Added CSS */
-        @media screen and (min-width: 600px) {
-            form {
-                margin: 20px auto;
-                max-width: 600px;
-                width: 100%;
-            }
-        }
-    </style>
+    }
+</style>
 </head>
 <body>
-    <h2>Email Form</h2>
-    <form method="post">
-        <label for="from">From:</label>
-        <input type="email" id="from" name="from" required>
-        
-        <label for="to">To:</label>
-        <input type="email" id="to" name="to" required>
-        
-        <label for="subject">Subject:</label>
-        <input type="text" id="subject" name="subject" required>
-        
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" rows="10" required></textarea>
-        
-        <input type="submit" value="Send Email">
-    </form>
+    <div class="container">
+        <h1>Email Form</h1>
+        <form method="post">
+            <label for="from">From</label>
+            <input type="email" id="from" name="from" required>
+            <label for="to">To</label>
+            <input type="email" id="to" name="to" required>
+            <label for="subject">Subject</label>
+            <input type="text" id="subject" name="subject" required>
+            <label for="message">Message</label>
+            <textarea id="message" name="message" rows="10" required></textarea>
+            <input type="submit" value="Send">
+            <p style="color: #fff;"><?php echo $statusMessage; ?></p>
+        </form>
+    </div>
 </body>
 </html>
